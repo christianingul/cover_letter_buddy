69d77548ac2b350a7ebb240284da201b407ff30f
Christian Magnus Bjoerk Ingul <106429512+christianingul@users.noreply.github.com>
2023-06-01 16:41:43 -0700

    Create company_scraper.py

---
+++ company_scraper.py
@@ -0,0 +1,21 @@
+from langchain.chains import LLMChain, LLMRequestsChain
+from constants import JOB_DESCRIPTION_TEMPLATE
+from langchain.chat_models import ChatOpenAI
+from langchain.prompts import PromptTemplate
+def scrape_company_page(company_url: str):
+
+    llm = ChatOpenAI(
+        temperature=0, model_name= 'gpt-3.5-turbo', openai_api_key="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"
+    )
+    PROMPT = PromptTemplate(
+        input_variables=["requests_result"],
+        template=JOB_DESCRIPTION_TEMPLATE,
+    )
+
+    chain = LLMRequestsChain(llm_chain=LLMChain(llm=llm, prompt=PROMPT))
+
+    inputs = {
+        "url": company_url,
+    }
+    result = chain.run(inputs)
+    return result
