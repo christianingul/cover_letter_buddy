dd284072a804ba13d6de95c9c45e60001e05cb97
Christian Magnus Bjoerk Ingul <106429512+christianingul@users.noreply.github.com>
2023-06-01 17:15:06 -0700

    Update company_scraper.py

--- company_scraper.py
+++ company_scraper.py
@@ -2,10 +2,14 @@ from langchain.chains import LLMChain, LLMRequestsChain
 from constants import JOB_DESCRIPTION_TEMPLATE
 from langchain.chat_models import ChatOpenAI
 from langchain.prompts import PromptTemplate
+import streamlit as st
+
 def scrape_company_page(company_url: str):
+    openai_secret = st.secrets.get("openai")
+    openai_api_key = openai_secret.get("key")
 
     llm = ChatOpenAI(
-        temperature=0, model_name= 'gpt-3.5-turbo', openai_api_key=#
+        temperature=0, model_name= 'gpt-3.5-turbo', openai_api_key=openai_api_key
     )
     PROMPT = PromptTemplate(
         input_variables=["requests_result"],
